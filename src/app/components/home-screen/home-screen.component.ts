670d5c25a0344803dc875860220d2b6f4485cb20
ThantzinTun007 <tzt.thantzintun2022@gmail.com>
2024-11-09 16:40:03 +0630

    navbar component

--- src/app/components/home-screen/home-screen.component.ts
+++ src/app/components/home-screen/home-screen.component.ts
@@ -3,21 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
-  styleUrl: './home-screen.component.css'
+  styleUrl: './home-screen.component.css',
 })
 export class HomeScreenComponent {
-  countries = [
-    { code: 'us', name: 'United States' },
-    { code: 'fr', name: 'France' },
-    { code: 'jp', name: 'Japan' },
-    // Add more countries as needed
-  ];
+  sidebarOpen: boolean = false;
 
-  // Default selected country
-  selectedCountry = 'us';
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 
-  // Method to handle country change
-  onCountryChange(countryCode: string) {
-    this.selectedCountry = countryCode;
+  closeToggle(): void {
+    this.sidebarOpen = false;
   }
 }
